8b92d9b4abafcfcf224db9510322f669b27409bd
Temitope <tope_olajide@outlook.com>
2020-01-06 05:47:15 +0100

    Create Footer.jsx

---
+++ src/components/commons/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+const Footer = () => {
+    return (
+        <>
+        <footer className="footer"><p>Copyright © 2019 My-Diary. All rights reserved | Designed with love by Temitope</p></footer>
+        </>
+    )
+}
+export default Footer
