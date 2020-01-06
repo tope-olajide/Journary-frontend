27a88fdbee7443fe040fbb11f51aba62ede118a1
Temitope <tope_olajide@outlook.com>
2020-01-06 05:47:25 +0100

    Create LoadingPage.jsx

---
+++ src/components/commons/LoadingPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+const LoadingPage = () => {
+     return (
+         <>
+        <section className ="loading-container">
+        <h1>Loading...</h1>
+        </section>
+         </>
+     )
+}
+export default LoadingPage
