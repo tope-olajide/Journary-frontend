f2cafbe8e5a95495e990057476384073985fa707
Temitope <tope_olajide@outlook.com>
2020-01-07 05:49:39 +0100

    Create SignIn Component

---
+++ src/components/SignIn/SignInView.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import AuthNav from "../commons/AuthNav";
+import Footer from "../commons/Footer";
+
+const SignInView = ({handleChange, handleFormSubmit, isLoading}) => {
+
+  return (
+    <>
+      <AuthNav />
+      <section className="form-section">
+        <form className="form-container">
+          <h1>Sign In</h1>
+          <input
+            type="text"
+            placeholder="username or email"
+            name="authName"
+            onChange={handleChange}
+          />
+          <input
+            type="password"
+            placeholder="password"
+            name="password"
+            onChange={handleChange}
+          />
+          <section className="form-button">
+            <button disabled={isLoading} onClick={handleFormSubmit}>
+            {isLoading ? "Logging in..." : "Sign in"}
+            </button>
+          </section>
+        </form>
+      </section>
+      <Footer />
+    </>
+  );
+};
+export default SignInView;
---
+++ src/components/SignIn/index.jsx
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import jsonwebtoken from "jsonwebtoken";
+import axios from "axios";
+import { Redirect } from "react-router-dom";
+import handleNetworkError from "../../utils/networkErrorHandler";
+import { Store } from "../../Store";
+import { ToastContainer, toast } from "react-toastify";
+import useForm from "../CustomHooks/useForm";
+import SignInView from './SignInView'
+const SignIn = () => {
+    const url = "http://127.0.0.1:9000/api/user/";
+    const [isLoading, setIsLoading] = useState(false);
+    const { inputs, handleChange } = useForm();
+    const { dispatch } = React.useContext(Store);
+    const [redirectToHomepage, setRedirectToHomepage] = useState(false);
+    const handleFormSubmit = async e => {
+        e.preventDefault();
+        try {
+            toast.info(`Signing you up...`, {
+              position: "bottom-left"
+            });
+            setIsLoading(true);
+            const response = await axios.post(`${url}signin`, inputs);
+            const { token } = response.data;
+            localStorage.setItem("token", token);
+            axios.defaults.headers.common.authorization = token;
+            toast.success(`Welcome back ${inputs.authName}`);
+            setTimeout(() => {
+              setRedirectToHomepage(true);
+            }, 1000);
+            setIsLoading(false);
+            const userData = jsonwebtoken.decode(localStorage.getItem("token"));
+            return dispatch({
+              type: "SET_CURRENT_USER",
+              userData
+            });
+        }catch (error) {
+        setIsLoading(false);
+        const errorMessage = handleNetworkError(error);
+        return toast.error(errorMessage, {
+          position: "bottom-left"
+        });
+      }
+    }
+    if (redirectToHomepage) {
+      return <Redirect to="/" />;
+    }
+    return (
+        <>
+<SignInView 
+handleFormSubmit={handleFormSubmit}
+handleChange={handleChange}
+isLoading={isLoading}
+/><ToastContainer />
+        </>
+    )
+}
+export default SignIn
