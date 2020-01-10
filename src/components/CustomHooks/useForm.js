beac265ce0328f5cb34b07d2a423a81fc4b578ab
Temitope <tope_olajide@outlook.com>
2020-01-10 14:45:42 +0100

    Update useForm.js

--- src/components/CustomHooks/useForm.js
+++ src/components/CustomHooks/useForm.js
@@ -1,14 +1,15 @@
-import { useState } from "react"
+import { useState } from "react";
 const useForm = (initial = {}) => {
-const [inputs, setInputs] = useState (initial);
-const handleChange =(e)=>{
+  const [inputs, setInputs] = useState(initial);
+  const handleChange = e => {
     setInputs({
-        ...inputs, [e.target.name]: e.target.value,
-    })
-}
-return {
-inputs,
-handleChange
-}
-}
-export default useForm
+      ...inputs,
+      [e.target.name]: e.target.value
+    });
+  };
+  return {
+    inputs,
+    handleChange
+  };
+};
+export default useForm;
