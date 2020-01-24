b9478263c4c5fac8da9690233faaa6d84097a9e4
Temitope <tope_olajide@outlook.com>
2020-01-24 05:59:14 +0100

    Update ImageGallery.jsx

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,45 +1,47 @@
-import React from 'react'
-import img1 from "../../images/8.jpeg";
-import img2 from "../../images/security.jpg";
-const ImageGallery =({selectImage})=> {
+import React from "react";
+import Image from 'react-graceful-image'
+import axios from 'axios';
+const ImageGallery = ({imageGalleries,deleteImage, addImage, toggleGalleryModal, isGalleryPage }) => {
+  const url = 'http://127.0.0.1:9000/api/user/gallery';
+  const selectImageForEditor = imageUrl => {
+    addImage(imageUrl);
+    toggleGalleryModal();
+  };
+  const onImageDeleteImage = async imageId => {
+    deleteImage(imageId)
+     console.log(imageId);
+/*    const response = await axios.get(`${url}/${imageId}`, setHeaderToken); */
 
-    return (
-        <>
-                <section className="image-gallery">
-                  <main>
-                    <img src={img1} alt={img1} />
-                    <button onClick={() =>selectImage('https://res.cloudinary.com/temitope/image/upload/v1557074023/gcv7rbem66pj1uobhh1q.jpg')} className="fit">Select</button>
-                  </main>
-                  <main>
-                    <img src={img2} alt={img2} />
-                    <button className="fit">Select</button>
-                  </main>
-                  <main>
-                    <img src={img2} alt={img2} />
-                    <button className="fit">Select</button>
-                  </main>
-                  <main>
-                    <img src={img2} alt={img2} />
-                    <button className="fit">Select</button>
-                  </main>
-                  <main>
-                    <img src={img1} alt={img1} />
-                    <button className="fit">Select</button>
-                  </main>
-                  <main>
-                    <img src={img2} alt={img2} />
-                    <button className="fit">Select</button>
-                  </main>
-                  <main>
-                    <img src={img2} alt={img2} />
-                    <button className="fit">Select</button>
-                  </main>
-                  <main>
-                    <img src={img2} alt={img2} />
-                    <button className="fit">Select</button>
-                  </main>
-                </section>
-        </>
-    )
-}
-export default ImageGallery
+  };
+
+
+  return (
+    <>
+      <div class="modal-body">
+        <section className="image-gallery">
+          {imageGalleries.map(image => {
+            return (
+              <main>
+                <Image src={image.image_url} alt={image.image_url} />
+                {isGalleryPage ? (
+                  <button
+                    onClick={() => onImageDeleteImage(image.gallery_id)}
+                    className="fit">
+                    Delete
+                  </button>
+                ) : (
+                  <button
+                    onClick={() => selectImageForEditor(image.image_url)}
+                    className="fit">
+                    Select
+                  </button>
+                ) }
+              </main>
+            );
+          })}
+        </section>
+      </div>
+    </>
+  );
+};
+export default ImageGallery;
