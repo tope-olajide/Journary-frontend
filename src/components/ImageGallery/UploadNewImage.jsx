5bddbbee38d211122b24bbec86a0eb5cff2b49dd
Temitope <tope_olajide@outlook.com>
2020-01-25 06:00:18 +0100

    Update UploadNewImage.jsx

--- src/components/ImageGallery/UploadNewImage.jsx
+++ src/components/ImageGallery/UploadNewImage.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
-import useImageUpload from "../CustomHooks/useSelectImageUpload";
-const UploadNewImage = ()=>{
-    const { getRootProps, getInputProps, files } = useImageUpload();
+import useImageUpload from "../CustomHooks/useImageUpload";
+import { ToastContainer, toast } from "react-toastify";
+const UploadNewImage = ()=> {
+const { getRootProps, getInputProps, files, imageUpload } = useImageUpload();
+    const uploadImage = async () => {
+      const {success, message} = await imageUpload(files);
+       if (success){
+        toast.success(message, {
+          position: "bottom-left"
+        });
+      }
+      else {
+        toast.error(message, {
+          position: "bottom-left"
+        })
+      }
+    }
     
     const thumbs = files.map(file => (
         <div className='thumb' key={file.name}>
@@ -11,18 +25,19 @@ const UploadNewImage = ()=>{
         </div>
       ));
     return (
-        <>
+      <>
 <aside className='thumbs-container'>{thumbs}</aside>
                 <div {...getRootProps({ className: "dropzone" })}>
                   <input {...getInputProps()} />
                   <div className="upload-info-container">
                     <h3>Drag 'n' drop pictures here</h3> <h3> or </h3>
-                    <h3>click to select files</h3>
+                    <h3>click to select </h3>
                   </div>
                 </div>
                 <div className="center">
-                  <button>Upload Image</button>
+                  <button onClick={uploadImage}>Upload Image</button>
                 </div>
+                <ToastContainer />
         </>
     )
 }
