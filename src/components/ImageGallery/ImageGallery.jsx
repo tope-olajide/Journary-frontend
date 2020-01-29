6f96be49fa02310b9f964ea1d00a3da5c3cd055e
Temitope <tope_olajide@outlook.com>
2020-01-29 02:55:37 +0100

    Update ImageGallery.jsx

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,31 +1,54 @@
 import React from "react";
 import Image from 'react-graceful-image'
 import axios from 'axios';
-const ImageGallery = ({imageGalleries,deleteImage, addImage, toggleGalleryModal, isGalleryPage }) => {
+import useSWR from "swr";
+const token = localStorage.getItem('token');
+const setHeaderToken = {
+  headers: {
+      authorization: token
+  }
+}
+const ImageGallery = ({ addImage, toggleGalleryModal, isGalleryPage }) => {
   const url = 'http://127.0.0.1:9000/api/user/gallery';
+  const deleteImage = async (imageId) => {
+    const response = await axios.delete(`${url}/${imageId}`, setHeaderToken);
+    revalidate()
+    console.log(response)
+  }
   const selectImageForEditor = imageUrl => {
     addImage(imageUrl);
     toggleGalleryModal();
   };
-  const onImageDeleteImage = async imageId => {
+  const onImageDelete = async imageId => {
     deleteImage(imageId)
      console.log(imageId);
-/*    const response = await axios.get(`${url}/${imageId}`, setHeaderToken); */
-
+  };
+  const fetcher = async (...args) => {
+      const response = await axios.get(`${args}`, setHeaderToken);
+      /*const { gallery } = response.data;
+       console.log({ gallery })
+        dispatch({
+          type: 'SET_IMAGE_GALLERY',
+          gallery
+      }); */ 
+      return response.data
   };
 
-
+  const  { data,revalidate, error }  = useSWR(url, fetcher);
+  console.log (data)
+  if (error) return <div>failed to load</div>
+  if (!data) return <div>loading...</div>
   return (
     <>
       <div class="modal-body">
         <section className="image-gallery">
-          {imageGalleries.map(image => {
+          {data.gallery.map(image => {
             return (
               <main>
                 <Image src={image.image_url} alt={image.image_url} />
                 {isGalleryPage ? (
                   <button
-                    onClick={() => onImageDeleteImage(image.gallery_id)}
+                    onClick={() => onImageDelete(image.gallery_id)}
                     className="fit">
                     Delete
                   </button>
