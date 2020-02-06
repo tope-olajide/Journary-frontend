8bd812524c9ca039d1480a1981fd406859fe96a9
Temitope <tope_olajide@outlook.com>
2020-02-06 17:03:46 +0100

    Create Reminder Component

---
+++ src/components/Reminder/index.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import ReminderView from './ReminderView'
+import axios from "axios";
+import useSWR from "swr";
+import { ToastContainer, toast } from "react-toastify";
+import handleNetworkError from "../../utils/networkErrorHandler";
+const token = localStorage.getItem("token");
+
+const setHeaderToken = {
+  headers: {
+    authorization: token
+  }
+};
+
+
+const Reminder = () => {
+    const [schedule, setSchedule] = useState("" );
+    const setReminder = schedule => {
+      setSchedule(schedule);
+      console.log(schedule);
+    };
+    const saveReminder= async ()=> {
+        try {
+            const url = `http://127.0.0.1:9000/api/user/set-reminder`;
+            await axios.post(`${url}`, {schedule}, setHeaderToken);
+            toast.success(`Reminder set successfully`);
+                    setTimeout(() => {
+                    }, 3000);
+                }
+                catch (error) {
+                    const errorMessage = handleNetworkError(error);
+                    return toast.error(errorMessage, {
+                      position: "bottom-left"
+                    });
+                  }
+    }
+    const fetcher = async (...args) => {
+        const response = await axios.get(`${args}`, setHeaderToken);
+        console.log(response.data.reminder)
+        setSchedule(response.data.reminder)
+        return response.data;
+      };
+      const url = `http://127.0.0.1:9000/api/user/get-reminder`;
+    const { data, error } = useSWR(url, fetcher);
+  if (error) return <div>failed to load</div>;
+  if (!data) return <div>loading...</div>;
+ return (
+     <>
+     <ReminderView setReminder={setReminder} schedule={schedule} saveReminder={saveReminder} />
+     <ToastContainer />
+     </>
+ )
+};
+export default Reminder;
