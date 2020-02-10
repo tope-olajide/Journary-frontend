82ba45ccfe423fe59c34d2855fdd5d51cec12761
Temitope <tope_olajide@outlook.com>
2020-02-10 14:52:21 +0100

    Update EditorView.jsx

--- src/components/Editor/EditorView.jsx
+++ src/components/Editor/EditorView.jsx
@@ -1,42 +1,37 @@
 import React, { useContext } from "react";
 import { Editor } from "react-draft-wysiwyg";
-import useImageUpload from "../CustomHooks/useImageUpload";
-import EntriesView from "../Entries/EntriesView";
-import MainNavigationBar from "../commons/MainNavigationBar"
-import dompurify from 'dompurify';
-import useEditor from '../CustomHooks/useEditor'
-import CustomImageToolbar from './CustomImageToolbar'
-
-const EditorView = ({onEditorStateChange, editorState, defaultEditorState}) => {
-  /* const {onEditorStateChange, editorState} = useEditor()  */
-  const sanitizer = dompurify.sanitize;
-
+import CustomToolbars from "./CustomToolbars";
+import { Store } from "../../Store";
+const EditorView = ({
+  onEditorStateChange,
+  editorState,
+  defaultEditorState
+}) => {
+  const { state } = useContext(Store);
   return (
-  <>
-    <section className="editor-container">
-      <Editor
-        wrapperClassName="editor-wrapper"
-        editorClassName="editor"
-        toolbarClassName="toolbar"
-        toolbar={{
-          /* inline: { inDropdown: true },
-      list: { inDropdown: true },
-      textAlign: { inDropdown: true },
-      link: { inDropdown: true }, history: { inDropdown: true },*/
-          image: { className: 'hide-default-image-icon' },
-        }}
-        toolbarCustomButtons={[<CustomImageToolbar />]}
-        editorState={editorState}
-        onEditorStateChange={onEditorStateChange}
-        defaultEditorState={defaultEditorState}
-      />
-    </section>
-    
-{/* 
-<div
-          style={{ minHeight: "200px" }}
-          dangerouslySetInnerHTML={{ __html: sanitizer(html) }}
-        /> */}
-  </>
-)};
+    <>
+      <section className="editor-container">
+        <Editor
+          wrapperClassName={
+            state.maximizeEditor ? "editor-wrapper-max" : "editor-wrapper-min"
+          }
+          editorClassName="editor"
+          toolbarClassName="toolbar"
+          toolbar={{
+            inline: { inDropdown: true },
+            list: { inDropdown: true },
+            textAlign: { inDropdown: true },
+            link: { inDropdown: true },
+            history: { inDropdown: true },
+            image: { className: "hide-default-image-icon" }
+          }}
+          toolbarCustomButtons={[<CustomToolbars />]}
+          editorState={editorState}
+          onEditorStateChange={onEditorStateChange}
+          defaultEditorState={defaultEditorState}
+        />
+      </section>
+    </>
+  );
+};
 export default EditorView;
