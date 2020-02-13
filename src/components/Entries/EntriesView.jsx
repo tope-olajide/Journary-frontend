28953f7f2c3473a384bfb4888080a69df3e930c8
Temitope <tope_olajide@outlook.com>
2020-02-13 06:52:41 +0100

    Update EntriesView.jsx

--- src/components/Entries/EntriesView.jsx
+++ src/components/Entries/EntriesView.jsx
@@ -1,23 +1,15 @@
 import React from 'react'
-import MainNav from '../commons/MainNavigationBar'
-import encryptionImage from '../../images/8.jpeg'
-import expression from '../../images/dairy-expression.jpg'
-import a_friendly_reminder from '../../images/a_friendly_reminder.png'
-import security from '../../images/security.jpg'
 import Image from 'react-graceful-image'
-const EntriesView = ({title, entry_image_url}) => {
+import { Link } from "react-router-dom";
+const EntriesView = ({title, entry_image_url, entry_id}) => {
     return (
-        <>
-{/* <MainNav />
-<section class="hero-image"></section> */}
-    
-      <section class="entry">
+        <><Link className="entry"  to={`/entry-details/${entry_id}`}>
       <Image className="column-img"alt={entry_image_url} src={entry_image_url} />
+      
         <div class="entry-title">
           <h1>{title}</h1>
         </div>
-      </section>
-    
+      </Link>
         </>
     )
 }
